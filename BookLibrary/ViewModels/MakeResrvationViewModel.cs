414b93e8191574e973299e7382de5f6a29366e22
Andrey <88864809+AndreyPinchukDeveloper@users.noreply.github.com>
2022-07-02 22:24:08 +0700

    Development Update #4
    - created base class for all ViewModels classes with method OnPropertyChanged(implemented interface INotifyPropertyChanged);
    - created a view models for MainWindow and controllers;
    - created a bindings view and view model.

---
+++ BookLibrary/ViewModels/MakeResrvationViewModel.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace BookLibrary.ViewModels
+{
+    public class MakeResrvationViewModel:ViewModelBase
+    {
+        #region TextBoxView
+        private string _username;
+        public string Username
+        {
+            get { return _username; }
+            set 
+            {
+                _username = value;
+                OnPropertyCHanged(nameof(Username));
+            }
+        }
+
+        private int _floorNumber;
+        public int FloorNumber
+        {
+            get { return _floorNumber; }
+            set
+            {
+                _floorNumber = value;
+                OnPropertyCHanged(nameof(FloorNumber));
+            }
+        }
+
+        private int _roomNumber;
+        public int RoomNumber
+        {
+            get { return _roomNumber; }
+            set
+            {
+                _roomNumber = value;
+                OnPropertyCHanged(nameof(RoomNumber));
+            }
+        }
+
+        private DateTime _startDate;
+        public DateTime StartDate
+        {
+            get { return _startDate; }
+            set
+            {
+                _startDate = value;
+                OnPropertyCHanged(nameof(StartDate));
+            }
+        }
+
+        private DateTime _endDate;
+        public DateTime EndDate
+        {
+            get { return _endDate; }
+            set
+            {
+                _endDate = value;
+                OnPropertyCHanged(nameof(EndDate));
+            }
+        }
+        #endregion
+
+        #region Commands
+        //TODO - implement command
+        public ICommand SubmitCommand { get; }
+        public ICommand CancelCommand { get; }
+        #endregion
+
+        public MakeResrvationViewModel()
+        {
+
+        }
+    }
+}
---
+++ BookLibrary/ViewModels/ViewModelBase.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel;
+
+namespace BookLibrary.ViewModels
+{
+    public class ViewModelBase : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        /// <summary>
+        /// This method is serving to tell UI what bindings is update
+        /// </summary>
+        protected void OnPropertyCHanged(string propertyName)//only for inheritors
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
